70af0117c6ac2759c1b49670177d71b32dd756c5
Paul Miell <paulm@mfg.gg>
2023-03-30 21:41:43 +0200

    Update GetNFTJSON.cs

--- Assets/Scripts/NFT/GetNFTJSON.cs
+++ Assets/Scripts/NFT/GetNFTJSON.cs
@@ -1,18 +1,36 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
+
+
+
 
 public class GetNFTJSON : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private UnityWebRequest request;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //public IEnumerator ViewAccount(string ref)
+    //{
+
+    //    using (request = new UnityWebRequest(ref))
+    //    {
+
+
+    //        yield return request.SendWebRequest();
+
+    //        //Returned result
+    //        if (request.result == UnityWebRequest.Result.Success)
+    //        {
+    //            ViewAccount viewAccount = JsonConvert.DeserializeObject<ViewAccount>(request.downloadHandler.text);
+
+    //        }
+    //        else
+    //        {
+    //            Debug.LogError(string.Format("API post error: {0}", request.error));
+    //        }
+    //    }
+    //}
 }
